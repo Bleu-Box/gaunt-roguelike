bd5e0fdd1b2d59d53244cb064a7965c093ee61c1
B-B <xxarcajethxx@gmail.com>
2017-12-13 15:38:40 -0600

    Added magical effects for attackers

--- src/attacker.cpp
+++ src/attacker.cpp
@@ -1,19 +1,42 @@
+#include <stdexcept>
 #include "main.h"
 
-Attacker::Attacker(float power): power(power) {}
+Attacker::Attacker(float power, int accuracy, const char* action): power(power), accuracy(accuracy), action(action), enchanted(false) {
+	// accuracy must be in range [0..100]
+	if(accuracy < 0 || accuracy > 100) {
+		throw std::invalid_argument("Last argument must be within range 0..100.");
+	}
+}
+
+void Attacker::setEffect(Effect::EffectType type, int duration) {
+	effectType = type;
+	effectDuration = duration;
+	enchanted = true;
+}
 
 void Attacker::attack(Actor* owner, Actor* target) {
-	if (target->destructible && !target->destructible->isDead()) {
-		// if the target can be hurt and is alive:
-		if (power-target->destructible->defense > 0) {
-			engine.gui->message(Gui::ATTACK, "%s attacks %s, inflicting \n %g damage.", owner->name, target->name, power-target->destructible->defense);
-		} else { 
-			engine.gui->message(Gui::ATTACK, "%s attacks %s, but it has \n no effect!", owner->name, target->name);            
+	TCODRandom* rand = TCODRandom::getInstance();
+	
+	if(target->destructible && !target->destructible->isDead()) {
+		if(rand->getInt(0, 100) <= accuracy) {
+		        if(enchanted) {
+			        target->addEffect(new Effect(effectType, effectDuration));
+			}
+			
+			if(power-target->destructible->defense > 0) {
+				engine.gui->message(Gui::ATTACK, "%s %s %s, inflicting %g damage.",
+						   owner->name, action, target->name, power-target->destructible->defense);
+			} else { 
+				engine.gui->message(Gui::ATTACK, "%s %s %s, but it has no effect!",
+						    owner->name, action, target->name);            
+			}
+		} else {
+			engine.gui->message(Gui::OBSERVE, "%s misses %s.", owner->name, target->name);
 		}
 		
 		target->destructible->takeDamage(target, power);		
 	} else {
 		// if the owner is trying to attack something that can't be harmed
-		engine.gui->message(Gui::OBSERVE, "%s hits %s.", owner->name,target->name);
+		engine.gui->message(Gui::OBSERVE, "%s %s %s.", owner->name, action, target->name);
 	}
 }
--- src/attacker.h
+++ src/attacker.h
@@ -2,7 +2,19 @@
 class Attacker {
  public:
 	float power;
+	int accuracy; // must be in range [0..100]
+	const char* action; // the action the attacker does to its target
 	
-	Attacker(float power);
+	Attacker(float power, int accuracy, const char* action);
+		
 	void attack(Actor* owner, Actor* target);
+	Effect::EffectType getEffectType() const { return effectType; }
+	int getEffectDuration() const { return effectDuration; }
+	bool isEnchanted() const { return enchanted; }
+	void setEffect(Effect::EffectType type, int duration);
+
+ private:
+	Effect::EffectType effectType; // for magical effects
+	int effectDuration;
+	bool enchanted; // flags whether or not attacker has effect
 };
